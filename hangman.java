955188d305402c71a81b83d2ef55c7fb59db42dd
CoreZen <danielg1000@gmail.com>
2022-06-18 14:59:29 +0300

    Added new mechanic + removed uneccesery calls.

--- hangman.java
+++ hangman.java
@@ -1,108 +1,120 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-public class hangman 
-{
-    public static int lives = 6;
-    public static String wordToGuess;
-    public static Scanner sc = new Scanner(System.in);
-    public static boolean gameOver = false;
-    public static char [] correctChars;
-    public static int correctCharsCount=0;
-    
-    //Main method
-    public static void main(String[] args) 
-    {
-        while(!gameOver)
-        {
+public class hangman {
+    static int lives = 6;
+    static String wordToGuess;
+    static Scanner sc = new Scanner(System.in);
+    static boolean gameOver = false;
+    static char[] correctChars;
+    static int correctCharsCount = 0;
+    static char[] wrongGuessedChars = new char[6];
+    static int wrongGuessedCharsCount = 0;
+    static Pattern whitelist = Pattern.compile("[a-zA-Z]");
+
+    // Main method
+    public static void main(String[] args) {
+        while (!gameOver) {
             System.out.println("Welcome to Hangman!");
             generateWord();
             gameLogic();
             System.out.println("Do you want to play again? (y/n)");
             char answer = sc.next().charAt(0);
-            if(answer == 'n')
-            {
+            if (answer == 'n') {
                 gameOver = true;
                 System.out.println("Thanks for playing!");
-            }else{
+            } else {
                 System.out.print("\033\143");
                 gameOver = false;
                 lives = 6;
                 correctCharsCount = 0;
+                wrongGuessedCharsCount = 0;
+                wrongGuessedChars = new char[wrongGuessedChars.length];
             }
         }
     }
-    //Main game logic
-    public static void gameLogic()
-    {
+
+    // Main game logic
+    static void gameLogic() {
         printHangman(lives);
         characterToShow();
         char guess;
-        while(true && lives > 0 && !gameOver)
-        {
-            System.out.print("Guess a letter: ");
-            guess=sc.next().charAt(0);
+        while (true && lives > 0 && !gameOver) {
+            // input goes here
+            guess = input();
+            // end of input
             System.out.print("\033\143");
-            if(wordToGuess.toLowerCase().contains(guess+""))
-            {
-                    System.out.println("Correct!");
-                    for(int i=0;i<wordToGuess.length();i++)
-                    {
-                        if(wordToGuess.toLowerCase().charAt(i)==guess && correctChars[i]!=guess)
-                        {
-                            correctChars[i]=guess;
-                            correctCharsCount++;
-                        }
+            if (wordToGuess.toLowerCase().contains(guess + "")) {
+                System.out.println("Correct!");
+                for (int i = 0; i < wordToGuess.length(); i++) {
+                    if (wordToGuess.toLowerCase().charAt(i) == guess && correctChars[i] != guess) {
+                        correctChars[i] = guess;
+                        correctCharsCount++;
                     }
-            }
-            else
-            {
+                }
+            } else {
                 System.out.println("Incorrect!");
+                wrongGuessedChars[wrongGuessedCharsCount] = guess;
+                wrongGuessedCharsCount++;
                 lives--;
             }
             printHangman(lives);
             characterToShow();
+            if (correctCharsCount == wordToGuess.length()) {
+                System.out.println("You won!");
+                gameOver = true;
+            } else if (lives == 0) {
+                System.out.println("You lost! The word is: " + wordToGuess);
+                gameOver = true;
+            }
         }
 
     }
-    // Shows the characters that are correct and the ones that are unknown
-    public static void characterToShow()
-    {
-        for(int i=0;i<wordToGuess.length();i++)
-        {
-            if(wordToGuess.toLowerCase().charAt(i)==correctChars[i])
-            {
-                System.out.print(correctChars[i]);
+    /* Takes input from user 
+     * checks if the caharacter is valid and if it has already been guessed.
+     * otherwise it returns the character.
+     */
+    static char input() {
+        System.out.println("Guess a letter: ");
+        char guess = sc.next().charAt(0);
+        if (whitelist.matcher(guess + "").matches()) {
+            if ((new String(wrongGuessedChars)).contains(guess + "") || (new String(correctChars)).contains(guess + "")) {
+                System.out.println("You already guessed that letter!");
+                return input();
             }
-            else
-            {
+            else{
+                return guess;
+            }
+        }
+        System.out.println("Invalid input!");
+        return input();
+    }
+
+    // Shows the characters that are correct and the ones that are unknown
+    static void characterToShow() {
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.toLowerCase().charAt(i) == correctChars[i]) {
+                System.out.print(correctChars[i]);
+            } else {
                 System.out.print("_");
             }
         }
         System.out.println("\n");
-     
-        if(correctCharsCount==wordToGuess.length())
-        {
-            System.out.println("You won!");
-            gameOver=true;
-        }
-        else if(lives==0)
-        {
-            System.out.println("You lost! The word is: " + wordToGuess);
-            gameOver=true;
-        }
+        System.out.println("Wrong guesses: " + Arrays.toString(wrongGuessedChars));
     }
 
     // generate a random word
-    public static void generateWord()
-    {
-     String [] wordlist = {"Test","Word","Hello","World","Java","Coding","Programming","Computer","Program","Hangman", "Tiger", "Cat", "Orange", "Pink", "Green"};
-     wordToGuess=wordlist[(int) (Math.random() * wordlist.length)];
-     correctChars= new char[wordToGuess.length()];
+    static void generateWord() {
+        String[] wordlist = { "Test", "Word", "Hello", "World", "Java", "Coding", "Programming", "Computer", "Program",
+                "Hangman", "Tiger", "Cat", "Orange", "Pink", "Green" };
+        wordToGuess = wordlist[(int) (Math.random() * wordlist.length)];
+        correctChars = new char[wordToGuess.length()];
     }
-   // prints the hangman 
-    public static void printHangman(int state)
-    {
-        switch(state){
+
+    // prints the hangman
+    static void printHangman(int state) {
+        switch (state) {
             case 6:
                 System.out.println(" _________");
                 System.out.println("|         |");
@@ -157,7 +169,7 @@ public class hangman
                 System.out.println("|");
                 System.out.println("|_________");
                 break;
-                case 0:
+            case 0:
                 System.out.println(" _________");
                 System.out.println("|         |");
                 System.out.println("|         0");
